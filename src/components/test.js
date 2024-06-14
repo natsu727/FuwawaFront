ff445a56729209daec35a6eb0aa2d5083ed0f2da
natsu727 <himajin0717@gmail.com>
2024-06-14 18:01:03 +0900

    機能の大枠完成

--- src/components/test.js
+++ src/components/test.js
@@ -1,53 +1,19 @@
-// import React, { useRef, useEffect } from 'react';
-
-// const cv=window.cv
-// const Test = () => {
-//     const videoRef = useRef(null);
-// 	// const canvas =document.getElementById("outputCanvas");
-//     useEffect(() => {
-//         const getVideoStream = async () => {
-//             try {
-//                 const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-//                 if (videoRef.current) {
-//                     videoRef.current.srcObject = stream;
-// 					// const width = videoRef.current.clientWidth;
-// 					// const height = videoRef.current.clientHeight;
-// 					// const src =new cv.Mat(height,width,cv.CV_8UC4)
-// 					// const dist =new cv.Mat(height,width,cv.CV_8UC1)
-// 					// cv.cvtColor(src,dist,cv.COLOR_RGB2GRAY);
-// 					// cv.imshow("outputCanvas",dist)
-//                 }
-//             } catch (error) {
-//                 console.error('Error accessing media devices.', error);
-//             }
-//         };
-
-//         getVideoStream();
-//     }, []);
-// 	// let cap = new cv.VideoCapture(videoRef)
-// 	// cv.imshow("outputCanvas",cap)
-//     return (
-//         <div>
-//             <video ref={videoRef} autoPlay hidden/>
-// 			<canvas id="outputCanvas"/>
-//         </div>
-//     );
-// };
-
-// export default Test;
-
 import React, { useRef, useEffect, useState } from 'react';
 import '@tensorflow/tfjs';
 import * as handpose from '@tensorflow-models/handpose';
-
+const status={
+  "waiting":"🔴",
+  "watching":"🟢"
+}
 const Test = () => {
-  const videoRef = useRef(null);
-  const canvasRef = useRef(null);
-  const [model, setModel] = useState(null);
-
+	const videoRef = useRef(null);
+	const canvasRef = useRef(null);
+	const [model, setModel] = useState(null);
+  const [message,setMessage] = useState("waiting")
+  const [cameraState,setCameraState] = useState(status.waiting) 
   useEffect(() => {
     const loadModel = async () => {
-      const handposeModel = await handpose.load();
+      const handposeModel = await handpose.load(60);
       setModel(handposeModel);
     };
     loadModel();
@@ -63,51 +29,54 @@ const Test = () => {
     };
     startVideo();
   }, []);
-
+  let prevX=325;
   useEffect(() => {
-    const detectHands = async () => {
-      if (videoRef.current && model) {
-        const predictions = await model.estimateHands(videoRef.current);
-		// console.log(predictions)
-        if (predictions.length > 0) {
-          drawHands(predictions);
+      const detectHands = async () => {
+        if (videoRef.current && model) {
+          const predictions = await model.estimateHands(videoRef.current);
+          if (predictions.length > 0) {
+            setCameraState(status.watching)
+            monitalingHands(predictions);
+          }else{
+            prevX=325;
+            setMessage("waiting")
+            setCameraState(status.waiting)
+          }
         }
-      }
-      requestAnimationFrame(detectHands);
-    };
-
-    const drawHands = (predictions) => {
-      const ctx = canvasRef.current.getContext('2d');
-      ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
-      predictions.forEach(prediction => {
-		  const landmarks = prediction.landmarks;
-		  let sum_x=0;
-		  let sum_y=0;
-		  landmarks.forEach(landmark => {
-			  const [x, y ,z] = landmark;
-			  sum_x+=x;
-			  sum_y+=y;
-			//   ctx.beginPath();
-			//   ctx.arc(x, y, 5, 0, 2 * Math.PI);
-			//   ctx.fillStyle = 'red';
-			//   ctx.fill();
-			  });
-			ctx.beginPath();
-			ctx.arc(sum_x/21, sum_y/21, 5, 0, 2 * Math.PI);
-			ctx.fillStyle = 'red';
-			ctx.fill();
-		});
-    };
-
+        requestAnimationFrame(detectHands);
+      };
+      const monitalingHands = (predictions) => {
+        predictions.forEach(prediction => {
+        const landmarks = prediction.landmarks;
+        let sum_x=0;
+        landmarks.forEach(landmark => {
+          const [x] = landmark;
+          sum_x+=x;
+          });
+          if(prevX-sum_x/21>100){
+            setMessage("次！！")
+            console.log("次")
+          }
+          else if(prevX-sum_x/21<-100){
+            setMessage("前！！")
+            console.log("前")
+          }
+          prevX=sum_x/21
+        });
+      };
     if (model) {
       detectHands();
     }
-  }, [model]);
+  // }
+    }, [model]);
+  
 
   return (
     <div>
-      <video ref={videoRef} autoPlay style={{display:'none'}}/>
-      <canvas ref={canvasRef} width="640" height="480" style={{ border: '1px solid black' }} />
+      {cameraState}
+      <h1>{message}</h1>
+      <video ref={videoRef} autoPlay style={{display:"none"}} />
+      {/* <canvas ref={canvasRef} width="640" height="480" style={{ border: '1px solid black' }} /> */}
     </div>
   );
 };
