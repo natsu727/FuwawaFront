04563ae109ebce38fd2e0a4a57728e1a5bbfc151
parari415 <parari415@gmail.com>
2024-06-16 03:53:10 +0900

    aaa

--- src/components/BookDetail.js
+++ src/components/BookDetail.js
@@ -1,24 +1,56 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import "../App.css";
+import "./BookDetail.css";
 
-// import React from 'react';
-// import Sample from './Sample';
+const BookDetail = () => {
+  const { id } = useParams();
+  const [content, setContent] = useState("");
+  const [visibleContent, setVisibleContent] = useState("");
+  const [currentPage, setCurrentPage] = useState(0);
 
-function BookDetail() {
-  const style = {
-    width: "50%",
-    margin: "0 auto",
-    marginTop: 150,
+  useEffect(() => {
+    fetch(`https://fuwawa-back2.onrender.com/book/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setContent(data.content);
+        setVisibleContent(data.content.substring(0, 200));
+      });
+  }, [id]);
+
+  const handleNextPage = () => {
+    const nextPage = currentPage + 1;
+    const startIndex = nextPage * 200;
+    const endIndex = startIndex + 200;
+    setVisibleContent(content.substring(startIndex, endIndex));
+    setCurrentPage(nextPage);
   };
 
-         // <Sample />
+  const handlePrevPage = () => {
+    if (currentPage > 0) {
+      const prevPage = currentPage - 1;
+      const startIndex = prevPage * 200;
+      const endIndex = startIndex + 200;
+      setVisibleContent(content.substring(startIndex, endIndex));
+      setCurrentPage(prevPage);
+    }
+  };
+
   return (
-    <div className="App">
-      <div style={style}>
+    <div className="book-detail">
+      <h2>本の詳細</h2>
+      <div className="book-content">
+        <p>{visibleContent}</p>
+      </div>
+      <div className="navigation-buttons">
+        <button onClick={handlePrevPage} disabled={currentPage === 0}>
+          前のページ
+        </button>
+        <button onClick={handleNextPage} disabled={currentPage * 200 >= content.length}>
+          次のページ
+        </button>
       </div>
     </div>
   );
-}
+};
 
 export default BookDetail;
